abf1d0209070add4a4a3c8301ff4a5957e125fcf
shoneriki <ikirenohs@gmail.com>
2020-11-02 11:01:50 +0900

    done with logic and minor css

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,17 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    
+    if params[:question] == "I am going to work"
+      @answer = "Great!"
+    elsif params[:question].include? ("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+    return @answer
+
+
   end
 
 end
